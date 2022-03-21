137c01c50b119ad1c664656b0fb6e266f51cf969
Jade <jade.cmail@gmail.com>
2022-03-21 10:25:43 +0100

    init commit

---
+++ src/main/java/com/checkpoint4/jcollection/JcollectionApplication.java
@@ -0,0 +1,13 @@
+package com.checkpoint4.jcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/checkpoint4/jcollection/JcollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkpoint4.jcollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
